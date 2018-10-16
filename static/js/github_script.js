10a8250e98b834a46ee35d984a75a6f9ae9901a9
Nicholas Reno Casteen <NickRCasteen@gmail.com>
2018-10-16 00:15:19 -0500

    Add files via upload

--- static/js/github_script.js
+++ static/js/github_script.js
@@ -2,14 +2,14 @@
 function refresh()
 {
     
+    resetBody();
     
     
     var request = new XMLHttpRequest();
     const app = document.getElementById('root');
-    const full = document.getElementById('full');
+    const table = document.getElementById('sorter_tab');
     
-    app.innerHTML = ""
-
+    
     request.open('GET', 'https://api.github.com/events', true);
 
     request.onload = function () {
@@ -18,68 +18,25 @@ function refresh()
         var data = JSON.parse(this.response);
 
         if (request.status >= 200 && request.status < 400) {
-            data.forEach(event => {
-      
-                //HERE IS THE MODAL. This will display 'more information'. 
-                const modal = document.createElement('div');
-                const modal_diag = document.createElement('div');
-                const modal_cont = document.createElement('div');
-                const modal_header = document.createElement('div');
-                const modal_body = document.createElement('div');
-                const mh4 = document.createElement('h4');
-                mh4.textContent = `${event.actor.display_login} on ${event.repo.name}`;
-                const mp = document.createElement('p');
-                mp.textContent = `Public: ${event.public}...Event Created: ${event.created_at}`;
-      
-                //Attribute setting for the modal
-                modal.setAttribute('id', `${event.id}`);
-                modal.setAttribute('class', 'modal fade');
-                modal.setAttribute('tabindex', '-1');
-                modal.setAttribute('role', 'dialog');
-                modal.setAttribute('aria-labelledby', `h_info${event.id}`);
-                modal.setAttribute('aria-hidden', 'true');
-      
-                modal_diag.setAttribute('class', 'modal-dialog');
-                //modal_diag.setAttribute('role', 'document');
-      
-                modal_cont.setAttribute('class', 'modal-content');
-      
-                modal_header.setAttribute('class', 'modal-header');
-                modal_header.setAttribute('id', `h_info${event.id}`);
-      
-                modal_body.setAttribute('class', 'modal-body');
-      
-                mh4.setAttribute('class', 'modal-title');
+            //Set up variables.
+            var col_count = 0; //The counter for columns in the grid.
             
-                //Modal assembly into Document
-                //document.body.insertBefore(modal, full);
-                document.body.appendChild(modal);
-                modal.appendChild(modal_diag);
-                modal_diag.appendChild(modal_cont);
-                modal_cont.appendChild(modal_header);
-                modal_cont.appendChild(modal_body);
-                modal_header.appendChild(mh4);
-                modal_body.appendChild(mp);
+            data.forEach(event => {
+                
+                if (col_count%3 === 0)
+                {
+                    var tempor = document.createElement('tr');
+                    table.appendChild(tempor);
+                }
+                //HERE IS THE MODAL. This will display 'more information'. 
+
+                modalCreation(event);
                 
                 //HERE IS THE PARENT BUTTON. It's given the card class and the ability
                 //to toggle modals.
-                const card = document.createElement('button');
-                card.setAttribute('class', 'card');
-                card.setAttribute('data-toggle', 'modal');
-                card.setAttribute('data-target', `#${event.id}`);
-                //card.innerHTML = mh4.textContent;
-                //Text
-                const h1 = document.createElement('p');
-                h1.textContent = mh4.textContent;
-                //Text
-                const h2 = document.createElement('h6');
-                h2.textContent = event.type;
+                cardCreation(event, tempor);
                 
-                //+++ APPENDING +++
-                //This is where we place our card and its children right where they belong.
-                app.appendChild(card);
-                card.appendChild(h1);
-                card.appendChild(h2);
+                col_count+=1;
                 
             });
         } 
@@ -89,4 +46,112 @@ function refresh()
     };
 
     request.send();
+}
+
+
+
+function modalCreation(event)
+{
+    //COMPONENT CREATION
+    const modal = document.createElement('div');
+    const modal_diag = document.createElement('div');
+    const modal_cont = document.createElement('div');
+    const modal_header = document.createElement('div');
+    const modal_body = document.createElement('div');
+    const mh4 = document.createElement('h4');
+    const mp = document.createElement('p');
+      
+    //MAIN MODAL ATTRIBUTE SET
+    modal.setAttribute('id', `${event.id}`);
+    modal.setAttribute('class', 'modal fade');
+    modal.setAttribute('tabindex', '-1');
+    modal.setAttribute('role', 'dialog');
+    modal.setAttribute('aria-labelledby', `h_info${event.id}`);
+    modal.setAttribute('aria-hidden', 'true');
+    
+    //CHILD ATTRIBUTE SET
+    modal_diag.setAttribute('class', 'modal-dialog');
+      
+    //GRANDCHILD ATTRIBUTE SET
+    modal_cont.setAttribute('class', 'modal-content');
+      
+    //G.GRANDCHILD #1 ATTRIBUTE SET
+    modal_header.setAttribute('class', 'modal-header');
+    modal_header.setAttribute('id', `h_info${event.id}`);
+      
+    //G.GRANDCHILD #2 ATTRIBUTE SET
+    modal_body.setAttribute('class', 'modal-body');
+      
+    mh4.setAttribute('class', 'modal-title');
+    mh4.textContent = `${event.actor.display_login} on ${event.repo.name}`;
+    
+    mp.textContent = `Public: ${event.public}<br>Event Created: ${event.created_at}`;
+            
+    //MODAL TREE ASSEMBLY
+    document.body.appendChild(modal);
+    modal.appendChild(modal_diag);
+    modal_diag.appendChild(modal_cont);
+    modal_cont.appendChild(modal_header);
+    modal_cont.appendChild(modal_body);
+    modal_header.appendChild(mh4);
+    modal_body.appendChild(mp);
+}
+
+
+function cardCreation(event, row)
+{
+    //ELEMENT CREATION
+    const card = document.createElement('button');
+    const cell = document.createElement('td');
+    
+    //CARD ATTRIBUTES SET
+    card.setAttribute('class', 'card');
+    card.setAttribute('data-toggle', 'modal');
+    card.setAttribute('data-target', `#${event.id}`);
+    
+    //HEADER TEXT
+    const h1 = document.createElement('p');
+    h1.textContent = `${event.actor.display_login} on ${event.repo.name}`;
+    
+    //MAIN TEXT
+    const h2 = document.createElement('h6');
+    h2.textContent = event.type;
+
+    //+++ APPENDING +++
+    //This is where we place our card and its children right where they belong.
+    row.appendChild(cell);
+    cell.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(h2);
+}
+
+
+function resetBody()
+{
+    document.body.innerHTML = `<div class="container">
+        <!-- THIS IS WHERE THE MODALS WILL GO-->
+        <div class="col-md-3">
+                <button type="button" class="btn btn-primary" onclick='refresh()'>
+                    Refresh
+                </button>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-9" id="root">
+                <table id='sorter_tab'></table>
+            </div>
+            
+        </div>
+    </div>
+    
+    
+    <footer>
+        <div class="container footercontainer">
+            <div class="col-md-12">
+                <p class="copyright">Copyright &copy; Nicholas Casteen 2018.</p>
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript" src="/static/js/all.min.js"></script>
+    <script type="text/javascript">refresh()</script>"`;
 }
